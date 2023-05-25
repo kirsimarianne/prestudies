b29d6e82cd98a9f3c7e85bedfa8cb9412f01d24e
Kirsi Huhtakallio <kirsi.huhtakallio@gmail.com>
2023-05-25 18:38:18 +0300

    add main wich calculates is gym membership better thanm single payemenmt for a year

---
+++ gym_membership.cpp
@@ -0,0 +1,28 @@
+
+// Exercise 2.3.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter the price for the memebrship card: ";
+    double price_mbrship {};
+    std::cin >> price_mbrship;
+    std::cout << "Enter the price for a single visit: ";
+    double price_single {};
+    std::cin >> price_single;
+    std::cout << "Enter the amount of times visiting to the gym in a year" << std::endl;
+    int visits {};
+    std::cin >> visits;
+
+    if(price_single * visits >= price_mbrship)
+    {
+        std::cout << "It is worth to buy a memebership." << std::endl;
+    }
+    else
+    {
+        std::cout << "It is not worth to buy a memebership." << std::endl;
+    }
+
+    return 0;
+}
